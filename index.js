8aaa9e76a1b132f59e2a9f6b7f3dc28cf70c9d04
shivraj sagar <shivsagar.sagar294@gmail.com>
2021-02-06 16:01:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+//import Setup from './screens/Setup';
+
+AppRegistry.registerComponent(appName, () => App);
